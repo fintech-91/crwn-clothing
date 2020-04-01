616ffb65014adc9901a0865d07597921dc19831c
fintech-91 <fintech91@gmail.com>
2020-04-01 10:08:07 +0530

    update menuItems to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './components/pages/homepage/homepage.component';
 
+
+
+const HatsPage = () => {
+  return (
+  <div>
+  <h1>HAT PAGE</h1>
+  </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route extact path='/' component={HomePage}/>
+      <Route path='/hats' component={HatsPage}/>
     </div>
   );
 }
